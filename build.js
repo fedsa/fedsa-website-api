6fd24477c6200fa6204338c17af920921a7de24f
Schalk Venter <venterschalk@gmail.com>
2022-11-28 13:20:16 +0200

    Adds members to API

--- build.js
+++ build.js
@@ -1,14 +1,33 @@
-// const puppeteer = require('puppeteer')
-const chromium = require('chrome-aws-lambda')
+
+const puppeteer = require('puppeteer')
+// const chromium = require('chrome-aws-lambda')
 const fs = require('fs')
 
-const init = async () => {
-  const browser = await chromium.puppeteer.launch({
-    args: await chromium.args,
-    executablePath: await chromium.executablePath,
-    headless: true,
-  })
+const headless = puppeteer.launch({
+  // args: await chromium.args,
+  // executablePath: await chromium.executablePath,
+  // headless: true,
+})
 
+const getLanding = async () => {
+  const browser = await headless
+  const page = await browser.newPage();
+  await page.goto("https://www.meetup.com/fedsa-community/");
+
+  const resultsSelector = "#members span"
+  await page.waitForSelector(resultsSelector);
+
+  const members = await page.evaluate((resultsSelector) => {
+    return parseInt(document.querySelector(resultsSelector).innerText.replace(/^Members\s\(/i, '').replace(/\)$/i, '').replace(/,/ig, ''))
+  }, resultsSelector)
+
+  return {
+    members
+  }
+}
+
+const getMeetups = async () => {
+  const browser = await headless
   const page = await browser.newPage();
   await page.goto("https://www.meetup.com/fedsa-community/events/");
 
@@ -26,8 +45,27 @@ const init = async () => {
     }));
   }, resultsSelector);
 
+  return {
+    events: {
+      upcoming,
+    }
+  }
+}
+
+
+const init = async () => {
+  const promises = await Promise.all([getLanding(), getMeetups()])
+  const [{ members }, { events }] = promises
+
+  const response = {
+    members,
+    events
+  }
+
+  fs.writeFileSync('./api.json', JSON.stringify(response, null, 2))
+
+  const browser = await headless
   await browser.close();
-  fs.writeFileSync('./api.json', JSON.stringify({ meetup: { upcoming } }))
 };
 
 init()
